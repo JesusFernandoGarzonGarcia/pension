4a66d98965cc3bbce3f2080718081924c9f0d0f8
Jesus Fernando Garzon <jesus.garzon@uptc.edu.co>
2022-03-30 16:28:50 -0500

    23467

--- admin/includes/dbcontroller.php
+++ admin/includes/dbcontroller.php
@@ -5,7 +5,7 @@ $DB_pass = "vzKTAoGQctvrmZ3BuEoG";
 $DB_name = "bhxcvvnq9j6lo7aynk87";
 try
 {
- $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+ $DB_con = new PDO("mysql://u5kqcja6qbfwixja:vzKTAoGQctvrmZ3BuEoG@bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com:3306/bhxcvvnq9j6lo7aynk87");
  $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
