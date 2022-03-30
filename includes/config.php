3684e5d371ccb2a64ca6dc523071811f94efa977
Jesus Fernando Garzon <jesus.garzon@uptc.edu.co>
2022-03-30 15:11:32 -0500

    hh

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
+$host="bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com:3306";
 $dbuser="u5kqcja6qbfwixja";
 $dbpass="vzKTAoGQctvrmZ3BuEoG";
-$host="bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com:3306";
 $db="bhxcvvnq9j6lo7aynk87";
 
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
